2d88f3765227526e50011897ddcabb161e5be5f5
Mohammad Marmash <muhammadmarmash@gmail.com>
2026-06-24 19:17:47 +0300

    Add Terraform security group module (SSH, HTTP/S, app port, egress)

---
+++ terraform/modules/security_group/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Main security group"
+  vpc_id      = var.vpc_id
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    name        = "${var.project_name}-${var.environment}-sg"
+  }
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.my_ip}/32"]
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "http_https" {
+  count             = length(var.allowed_ports)
+  type              = "ingress"
+  from_port         = var.allowed_ports[count.index]
+  to_port           = var.allowed_ports[count.index]
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = aws_security_group.main.id
+}
+
+resource "aws_security_group_rule" "outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main.id
+}
---
+++ terraform/modules/security_group/variables.tf
@@ -0,0 +1,31 @@
+variable "vpc_id" {
+  description = "The ID of the VPC where the security group will be created"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your IP address for SSH access"
+  type        = string
+}
+
+variable "allowed_cidr_blocks" {
+  description = "List of CIDR blocks allowed for HTTP and HTTPS access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+
+}
+variable "allowed_ports" {
+  description = "List of ports allowed for HTTP and HTTPS access"
+  type        = list(number)
+  default     = [80, 443, 5000]
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, staging, prod)"
+  type        = string
+}
