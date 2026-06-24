871dcd831a9af0eccb044feb218af2abd95ddece
Mohammad Marmash <muhammadmarmash@gmail.com>
2026-06-24 19:17:47 +0300

    Add Terraform VPC module (VPC, IGW, public subnet, route table)

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,31 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-vpc"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
