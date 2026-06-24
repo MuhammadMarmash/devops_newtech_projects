5d0048f7ac44612e6fb18902e9d3a719472a1e88
Mohammad Marmash <muhammadmarmash@gmail.com>
2026-06-24 19:17:47 +0300

    Wire up root Terraform config calling vpc, security_group and ec2 modules

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "vpc_module" {
+  source       = "./modules/vpc"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "security_group_module" {
+  source       = "./modules/security_group"
+  vpc_id       = module.vpc_module.vpc_id
+  my_ip        = var.my_ip
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "ec2_module" {
+  source            = "./modules/ec2"
+  security_group_id = module.security_group_module.security_group_id
+  subnet_id         = module.vpc_module.public_subnet_ids
+  project_name      = var.project_name
+  environment       = var.environment
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "my_ip" {
+  description = "Your IP address"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, staging, prod)"
+  type        = string
+}
